fcf48668021f9d70e774e0ea83b166f96e08b89e
Brunodev09 <bruno.mayol.giannotti@gmail.com>
2019-12-31 20:10:19 -0300

    Added more files as templates and tested other cfgs.

--- src/Main.ts
+++ src/Main.ts
@@ -100,6 +100,7 @@ export default class Main {
     }
     // @TODO - Tipefy the JSON template. 
     // @TODO - Add the option to process everything inside current dir instead of creating a new folder everytime.
+    // @TODO - Make creation of template files in ./dist automatic
     async select(): Promise<any> {
         let choices = [];
         let projects = {};
