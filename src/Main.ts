19d5d70a2cc324b799ec7afd167f39e30de52f43
Brunodev09 <bruno.mayol.giannotti@gmail.com>
2020-01-08 21:57:21 -0300

    Done message.

--- src/Main.ts
+++ src/Main.ts
@@ -220,6 +220,7 @@ export default class Main {
                 }
             }
         }
+        _.say('Done!');
     }
 
     async checkTemplatesFolderForMatchingFile(fileName: string): Promise<boolean> {
